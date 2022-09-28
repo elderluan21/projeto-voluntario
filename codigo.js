32c648678eac6161aac45cabb16ef867a8b82309
elderluan21 <101297926+elderluan21@users.noreply.github.com>
2022-09-28 11:16:14 -0400

    finalizaçao parte l

---
+++ codigo.js
@@ -0,0 +1,7 @@
+function doar(){
+    var doaçao = document.querySelector("#botao-doaçao").value
+    var nome = document.querySelector("#nome").value
+    alert("Olha, que otimo que deseja ajudar senhor(a) "+ nome + "  ,você pode fazer uma doaçao anônima agora, de qualquer valor para nos ajudar! este é o nosso pix:          , ficamos felizes e agradecemos sua ajuda.")
+}
+var contato = document.querySelector("#num-contato").value
+var email = document.querySelector("#email").value
